e820cb617707dc9a19577dbe4ce6c3d5d9173581
QQ喵 <gqqnb2005@gmail.com>
2020-02-22 17:58:26 -0800

    补上测试用例

---
+++ testcases/hw2/NullCheckSimple.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] a){
+        System.out.println(new A().id());
+    }
+}
+
+class A {
+    A f;
+    public int id(){
+        return f.id();
+    }
+}
