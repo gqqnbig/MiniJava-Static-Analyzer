98539ecced3cacb937e8078c8ce2008253a83ffb
QQ喵 <gqqnb2005@gmail.com>
2020-02-23 15:36:46 -0800

    添加 Pair9Test

---
+++ src/test/java/Pair9Test.java
@@ -0,0 +1,40 @@
+import baseVisitors.ArrayLookupVisitor;
+import baseVisitors.MessageSendCollector;
+import nullPointerAnalysis.*;
+import org.junit.Assert;
+import org.junit.Test;
+import syntaxtree.ArrayLookup;
+import syntaxtree.Goal;
+import syntaxtree.MessageSend;
+import typeAnalysis.ClassHierarchyAnalysis;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Pair9Test
+{
+	@Test
+	public void test() throws ParseException, FileNotFoundException
+	{
+		FileInputStream stream = new FileInputStream("testcases/hw2/Pair9.java");
+		try {MiniJavaParser.ReInit(stream);} catch (Throwable e) {new MiniJavaParser(stream);}
+		Goal goal = MiniJavaParser.Goal();
+
+		ProgramStructureCollector.init(goal);
+		ClassHierarchyAnalysis.init(goal);
+		PrintStream debugOut = new PrintStream(OutputStream.nullOutputStream());
+		ConstraintCollector constraintCollector = new ConstraintCollector();
+		goal.accept(constraintCollector, null);
+
+		List<EqualityRelationship> solutions = Solver.solve(constraintCollector.constraints);
+		solutions = solutions.stream().filter(r -> r.left instanceof VariableRes).collect(Collectors.toList());
+
+		Assert.assertFalse("Pair9.java doesn't throw null pointer exception.", Solver.checkNullPointerException(goal, solutions));
+
+	}
+}
