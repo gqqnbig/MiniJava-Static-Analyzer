37850ba1cde2c6c7aaa8753c34bf1a936b1e1f8b
QQ喵 <gqqnb2005@gmail.com>
2020-03-14 15:48:41 -0700

    VariableCollector算是辅助工具，移动到 NullCheck 内部

--- src/main/java/NullCheck.java
+++ src/main/java/NullCheck.java
@@ -1,14 +1,15 @@
 import baseVisitors.AllocationVisitor;
-import nullPointerAnalysis.ProgramStructureCollector;
-import nullPointerAnalysis.Solver;
-import syntaxtree.Goal;
+import baseVisitors.VoidScopeVisitor;
+import nullPointerAnalysis.*;
+import syntaxtree.*;
 import typeAnalysis.ClassHierarchyAnalysis;
-import utils.FlowSensitiveVariable;
-import utils.Options;
+import utils.*;
 
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class NullCheck
 {
@@ -64,3 +65,113 @@ public class NullCheck
 
 
 }
+
+
+/**
+ * Collect mathematical variables for solving constraints.
+ * The variable doesn't refer to variables in programming.
+ */
+class VariableCollector extends VoidScopeVisitor<Location>
+{
+	ArrayList<FlowSensitiveVariable> variables = new ArrayList<>();
+
+	List<ObjectIdentifierDefinition> nullablesInScope;
+
+	@Override
+	public void visitScope(MainClass n, Location argu)
+	{
+//		n.f14.accept(this, null);
+
+		nullablesInScope = ProgramStructureCollector.getNullableIdentifiersInScope(new Scope(getClassName(), getMethodName()));
+		n.f15.accept(this, null);
+	}
+
+	@Override
+	public void visitScope(MethodDeclaration n, Location argu)
+	{
+//		n.f7.accept(this, null);
+
+		nullablesInScope = ProgramStructureCollector.getNullableIdentifiersInScope(new Scope(getClassName(), getMethodName()));
+		n.f8.accept(this, null);
+
+		//return statement
+		Location returnLocation = new Location(n.f9);
+		for (ObjectIdentifierDefinition nullable : nullablesInScope)
+		{
+			variables.add(new VariableIn(nullable, returnLocation));
+			variables.add(new VariableOut(nullable, returnLocation));
+		}
+
+		n.f10.accept(this, new Location(n.f9));
+	}
+
+	@Override
+	public void visitScope(ClassDeclaration n, Location argu)
+	{
+		if (AllocationVisitor.usedClasses.contains(getClassName()))
+			n.f4.accept(this, null);
+	}
+
+	@Override
+	protected void visitScope(ClassExtendsDeclaration n, Location argu)
+	{
+		if (AllocationVisitor.usedClasses.contains(getClassName()))
+			n.f6.accept(this, null);
+	}
+
+
+	@Override
+	public void visit(MessageSend n, Location argu)
+	{
+		variables.add(new VariableRes(n, argu));
+		super.visit(n, argu);
+	}
+
+	@Override
+	public void visit(Statement n, Location argu)
+	{
+//		List<utils.ObjectIdentifierDefinition> nullables = NullableCollector.getNullableIdentifierInScope(new Scope(getClassName(), getMethodName()));
+
+		Location location = new Location(n);
+		for (ObjectIdentifierDefinition nullable : nullablesInScope)
+		{
+			variables.add(new VariableIn(nullable, location));
+			variables.add(new VariableOut(nullable, location));
+		}
+
+		super.visit(n, location);
+	}
+
+	@Override
+	public void visit(Expression n, Location argu)
+	{
+		assert argu != null;
+
+		//if (n.f0.choice instanceof AllocationExpression == false)
+		//{
+		VariableRes vRes = new VariableRes(n, argu);
+		variables.add(vRes);
+		//}
+		super.visit(n, argu);
+	}
+
+	@Override
+	public void visit(PrimaryExpression n, Location argu)
+	{
+		if (n.f0.choice instanceof Identifier)
+			variables.add(new VariableRes(n, argu));
+		else if (n.f0.choice instanceof NotExpression)
+			variables.add(new VariableRes(n, argu));
+		else if (n.f0.choice instanceof BracketExpression)
+			variables.add(new VariableRes(n, argu));
+
+		super.visit(n, argu);
+	}
+
+	@Override
+	public void visit(AllocationExpression n, Location argu)
+	{
+		variables.add(new VariableRes(n, argu));
+	}
+}
+
